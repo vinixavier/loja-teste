26bcd2a64e17b8c8db6be03141c64ea2324e8574
Andre Evangelista <andreluisce@MBP-de-Andre.Home>
2022-08-12 16:09:19 +0100

    funcionalidade ao botao para adicionar na lista de produtos

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-var $lista = document.querySelector('ul');
+var lista_produto = document.querySelector('#lista_produto');
 var $produto = document.querySelector('#produto');
-var $botao = document.querySelector('#btn');
 
-confirm('Você está entrando na loja do Palmeiras!!!');
+// var botao = document.querySelector('#btn_p1'); // DOM - Document Object Model
+
+// botao.addEventListener('click', function onClick(produto) {
+//     console.log(this.textContent);
+// })
+
+
+function AdicionarProdutoNoCarrinho(nomeProduto, precoProduto) {
+    var li = document.createElement('li');
+    li.innerText = nomeProduto + " - " + precoProduto;
+    lista_produto.appendChild(li);
+
+    // console.log('PRODUTO ADICIONANDO', nomeProduto, precoProduto);
+}
+
+// console.log("MEU BOTAO", botao);
+// Estudar o que são seletores (Selectors)
+
+// CSS 
+
+// confirm('Você está entrando na loja do Palmeiras!!!');
 
 
